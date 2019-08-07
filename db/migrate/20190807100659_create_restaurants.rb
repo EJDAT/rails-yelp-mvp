ff9cf5a51acf1dc9b18f2fa5dd1d3afd51f3998d
ejdataras <ejdaras@gmail.com>
2019-08-07 14:50:52 +0200

    reviewadded

--- db/migrate/20190807100659_create_restaurants.rb
+++ db/migrate/20190807100659_create_restaurants.rb
@@ -2,7 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.text :address
+      t.string :address
+      t.integer :phone_number
       t.string :category
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_093416) do
+ActiveRecord::Schema.define(version: 2019_08_07_124412) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.text "address"
+    t.string "address"
+    t.integer "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
